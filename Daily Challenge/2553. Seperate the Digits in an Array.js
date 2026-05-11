0427ffb81e6d20efe8f01dfd4813a46a972f8ec4
Devendra Gaud <devendragaud@externlabs.com>
2026-05-11 16:20:24 +0530

    refactor: optimize separateDigits function by using push instead on unshift for better performance

--- Daily Challenge/2553. Seperate the Digits in an Array.js
+++ Daily Challenge/2553. Seperate the Digits in an Array.js
@@ -3,15 +3,14 @@
  * @return {number[]}
  */
 var separateDigits = function(nums) {
-    let result = [], n=nums.length
+   let result = [],n=nums.length
     for(let i=n-1;i>=0;i--){
         let value = nums[i]
         while(value>0){
-            result.unshift(value%10)
+            result.push(value%10)
             value = Math.floor(value/10)
         }
     }
+    result.reverse()
     return result
-
-
 };
